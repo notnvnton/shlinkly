b16a31629d5387372b3a132e46d8e40e80d905b6
Anton Hodge <xlorine@Anton-Macbook-M1.local>
2026-06-10 22:49:01 +0200

    Initial Xcode multiplatform project setup

---
+++ Shlinkly/ShlinklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShlinklyApp.swift
+//  Shlinkly
+//
+//  Created by Anton Hodge on 10.06.26.
+//
+
+import SwiftUI
+
+@main
+struct ShlinklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
